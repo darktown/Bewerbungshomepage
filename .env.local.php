b3cd9267c58162da98e491c85ff8dadb64610aed
darktown <tiefflieger1981@googlemail.com>
2023-11-19 18:04:09 +0100

    Update .env.local.php

--- .env.local.php
+++ .env.local.php
@@ -5,5 +5,5 @@
 return array (
   'APP_ENV' => 'dev',
   'APP_SECRET' => '4d4f45e6c79aecc349cffbf6ffdfd857',
-  'DATABASE_URL' => 'mysql://x1v7euvf_rene:JxLQFt2R1lw^tAH2@127.0.0.1:3306/x1v7euvf_rene?serverVersion=5.7.24&charset=utf8mb4',
+  'DATABASE_URL' => 'privat4',
 );
